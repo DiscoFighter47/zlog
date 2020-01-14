2c76a51dbc7d4f68b1128417345f9b9d51ca1838
Zahid Al Tair <mdzahidh119@gmail.com>
2020-01-14 12:08:43 +0600

    fix gls cleanup to defer

--- gen_req_id.go
+++ gen_req_id.go
@@ -11,7 +11,7 @@ import (
 func GenReqID(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		gls.Set("request_id", uuid.NewV4().String())
+		defer gls.Cleanup()
 		next.ServeHTTP(w, r)
-		gls.Cleanup()
 	})
 }
