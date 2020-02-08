dc2c2a482e49fdf050bc88e565c20914555c268b
Zahid Al Tair <mdzahidh119@gmail.com>
2020-02-08 19:52:39 +0600

    refactor pkg footprint

--- gen_req_id.go
+++ gen_req_id.go
@@ -3,13 +3,17 @@ package zlog
 import (
 	"net/http"
 
+	uuid "github.com/satori/go.uuid"
 	"github.com/tylerb/gls"
 )
+
+// ReqIDTag ...
+const ReqIDTag = "request_id"
 
 // GenReqID ...
 func GenReqID(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		SetReqID()
+		gls.Set(ReqIDTag, uuid.NewV4().String())
 		defer gls.Cleanup()
 		next.ServeHTTP(w, r)
 	})
--- rid_hook.go
+++ rid_hook.go
@@ -3,13 +3,15 @@ package zlog
 import (
 	"fmt"
 
+	"github.com/tylerb/gls"
+
 	"github.com/rs/zerolog"
 )
 
 type ridHook struct{}
 
 func (h ridHook) Run(e *zerolog.Event, level zerolog.Level, msg string) {
-	if id := GetReqID(); id != "" && level != zerolog.NoLevel {
+	if id := gls.Get(ReqIDTag); id != nil && level != zerolog.NoLevel {
 		e.Str("request_id", fmt.Sprintf("%v", id))
 	}
 }
